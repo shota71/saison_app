9a41bc2b9f50284a48e56e102ffe84220a94a681
grifis <nekogrifis@gmail.com>
2023-03-31 18:16:56 +0900

    a

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -20,7 +20,6 @@ module Api
         posts = Post.order(created_at: :desc)
         @items = posts.as_json(include: { images: { only: [:url] }, user: {} }).map do |post|
           post.merge({ likes_count: post.favorites.count })
-        end
         post_like_ranks = Post.joins('LEFT OUTER JOIN favorites ON posts.id = favorites.post_id')
                               .select('posts.*, COUNT(favorites.id) as likes_count')
                               .group('posts.id')
